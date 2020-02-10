9cd0a834679d17ac16514292ac1d48af70da92af
susankoju <aksusankoju@gmail.com>
2020-02-10 23:12:27 +0545

    circle init

---
+++ back-end/configs/db.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	//host: 'db4free.net',
+    
+//	// user: process.env.dbUser || 'root',
+    
+//	// password: process.env.dbPw || '',
+    //user: 'lusers',
+    //password: 'mypassword',
+    //database: 'messageall',
+    //insecureAuth: true,
+	//
+	
+    host: 'db',
+    user: 'user',
+    password: 'root',
+    port: '3306',
+    database: 'messageAll',
+    //insecureAuth: true,
+    //socketPath: '/tmp/mysql.sock',
+}
